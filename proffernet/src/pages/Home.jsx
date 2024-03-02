4f909c67701753f961a1af6a0c69fb3b49da05d0
Alcyone47 <91411013+Alcyone47@users.noreply.github.com>
2024-03-02 20:12:52 +0530

    added footer to home page

--- proffernet/src/pages/Home.jsx
+++ proffernet/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../CSS/Home.css'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
 const Home = () => {
     const navigate = useNavigate();
@@ -9,6 +10,7 @@ const Home = () => {
         <div>
             Home
             <Navbar/>
+            <Footer/>
         </div>
     )
 }
