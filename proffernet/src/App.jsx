8134e13284cf17e1be018f964de3e84ffd189309
Alcyone47 <91411013+Alcyone47@users.noreply.github.com>
2024-03-03 05:24:19 +0530

    added events template

--- proffernet/src/App.jsx
+++ proffernet/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Events from './pages/Events';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp/>}/>
           <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/events" element={<Events/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ proffernet/src/pages/Events.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import '../CSS/Events.css'
+
+const Events = () => {
+
+    const navigate = useNavigate();
+
+    return (
+        <div>Events</div>
+    )
+}
+
+export default Events
