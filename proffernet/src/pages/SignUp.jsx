9a825c22acdd8ede1b0907461ffb09fa5a3fe408
Alcyone47 <91411013+Alcyone47@users.noreply.github.com>
2024-03-03 02:50:21 +0530

    signup page init

--- proffernet/src/pages/SignUp.jsx
+++ proffernet/src/pages/SignUp.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import '../CSS/SignUp.css'
 
 const SignUp = () => {
-    const navigate = useNavigate();
-    return (
-        <div>SignUp</div>
-    )
+  return (
+    <div>SignUp</div>
+  )
 }
 
 export default SignUp
